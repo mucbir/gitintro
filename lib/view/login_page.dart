807958af45e350a4e7dccf081fc2c78a18b37d35
mucbir abins <mucbir@gmail.com>
2022-04-07 09:45:36 +0700

    update splash_screen dan login_page

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:git_intro/view/register_page.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,13 +7,88 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("Login"),
-          Image.asset("assets/auth/img_login.png"),
-          Text("data"),
-          Text("data")
-        ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(children: [
+            Text("Login"),
+            Image.asset("assets/auth/img_login.png"),
+            Text(
+              "Selamat Datang",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            Text(
+                "Selamat Datang di Aplikasi Widya Edu Aplikasi Latihan dan Konsultasi Soal"),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    primary: Colors.white,
+                    fixedSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      side: const BorderSide(
+                        color: Colors.blue,
+                        style: BorderStyle.solid,
+                        width: 1,
+                      ),
+                    )),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const RegisterPage()));
+                },
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Image.asset("assets/auth/ic_google.png"),
+                  const SizedBox(width: 10),
+                  const Text(
+                    "Masuk dengan Google",
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                ]),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    primary: Colors.black,
+                    fixedSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      side: const BorderSide(
+                        color: Colors.blue,
+                        style: BorderStyle.solid,
+                        width: 1,
+                      ),
+                    )),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const RegisterPage()));
+                },
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Image.asset("assets/auth/ic_apple.png"),
+                  const SizedBox(width: 10),
+                  const Text(
+                    "Masuk dengan Apple",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ]),
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
---
+++ lib/view/register_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.white,
+        title: Text(
+          "Yuk isi data diri",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
