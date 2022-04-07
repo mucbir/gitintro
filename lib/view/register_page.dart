78007d5af6b2090e5afa5dcdbc4c08e9afb314c1
mucbir abins <mucbir@gmail.com>
2022-04-07 13:14:45 +0700

    update register_page

--- lib/view/register_page.dart
+++ lib/view/register_page.dart
@@ -8,6 +8,7 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final nameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +18,97 @@ class _RegisterPageState extends State<RegisterPage> {
         title: Text(
           "Yuk isi data diri",
           style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Email"),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.white,
+                border: Border.all(
+                  color: const Color(0xFFD6D6D6),
+                ),
+              ),
+              child: TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  disabledBorder: InputBorder.none,
+                  border: InputBorder.none,
+                  hintText: "Isi Email Anda",
+                  hintStyle: TextStyle(
+                    color: Color(0XFFAAAAAA),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text("Nama Lengkap"),
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.white,
+                border: Border.all(
+                  color: const Color(0xFFD6D6D6),
+                ),
+              ),
+              child: TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  disabledBorder: InputBorder.none,
+                  border: InputBorder.none,
+                  hintText: "Isi Nama Lengkap Anda",
+                  hintStyle: TextStyle(
+                    color: Color(0XFFAAAAAA),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    primary: Color(0xff3A7FD5),
+                    fixedSize: const Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25),
+                      side: const BorderSide(
+                        color: Colors.blue,
+                        style: BorderStyle.solid,
+                        width: 1,
+                      ),
+                    )),
+                onPressed: () {
+                  print(nameController.text);
+                  //Navigator.of(context).push(MaterialPageRoute(
+                  //builder: (context) => const RegisterPage()));
+                },
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  //Image.asset("assets/auth/ic_apple.png"),
+                  const SizedBox(width: 10),
+                  const Text(
+                    "Daftar",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ]),
+              ),
+            ),
+          ],
         ),
       ),
     );
